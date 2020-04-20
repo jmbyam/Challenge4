fd5becf235388b51c9fdcdb44de8a664a11844b3
jmbyam <jmbyam@alaska.edu>
2020-04-19 23:27:48 -0800

    Health bar and damage. Not much luck with spawn point

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	public int maxHealth = 100;
+	public int currentHealth;
+	Vector3 StartPoint;
+	public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		StartPoint = transform.position;
+		currentHealth = maxHealth;
+		healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			TakeDamage(20);
+		}
+		if (currentHealth == 0){
+			transform.position = StartPoint;
+			currentHealth = 101;
+		}
+		if (currentHealth == 101){
+			TakeDamage(1);
+		}
+    }
+
+	void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+
+		healthBar.SetHealth(currentHealth);
+	}
+}
